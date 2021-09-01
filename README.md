5889c3287fb059392d8bd4b5dd0af2bc0ee4080a
mou-hao <hao.mou@outlook.com>
2021-09-01 23:06:34 +0800

    added comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a repo for random stuff like implementations of algorithms I come across or stuff I don't know where else to put.
+
+The `.cpp`s all have their `main()`. Just `g++ -o output_name src.cpp` and run.
--- kmp.cpp
+++ kmp.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+// KMP is an O(n) algorithm that searches a pattern W in a string S.
 
-vector<int> computeLPS(string pat) {
-    vector<int> lps (pat.size());
+// Compute a partial match table T,
+// where T[i] gives us the amount of skipping we need to do
+// when a mismatch happens when S[m+i] != W[i].
+// The next possible match will start at m+i-T[i],
+// and we do not need to check the first T[i] characters.
+// In practice, we just need to check whether S[m+i] == W[T[i-1]] if i > 0. 
+std::vector<int> computeLPS(std::string pat) {
+    std::vector<int> lps (pat.size());
     lps[0] = 0;
     int len = 0, i = 1;
     while (i < pat.size()) {
         if (pat[i] == pat[len]) {
             lps[i++] = ++len;
-        }
-        else {
+        } else {
             if (len) {
                 len = lps[len-1];
-            }
-            else {
+            } else {
                 lps[i++] = 0;
             }
         }
@@ -24,24 +28,22 @@ vector<int> computeLPS(string pat) {
     return lps;
 }
 
-void KMPsearch(string str, string pat) {
+void KMPsearch(std::string str, std::string pat) {
     if (!str.size() || !pat.size()) { return; }
-    vector<int> lps { computeLPS(pat) };
+    std::vector<int> lps { computeLPS(pat) };
     int i = 0, j = 0;
     while (i < str.size()) {
         if (str[i] == pat[j]) {
             ++i;
             ++j;
             if (j == pat.size()) {
-                cout << "Found pattern at index " << i-j << endl;
+                std::cout << "Found pattern at index " << i-j << "\n";
                 j = lps[j-1];
             }
-        }
-        else {
+        } else {
             if (j) {
                 j = lps[j-1];
-            }
-            else {
+            } else {
                 ++i;
             }
         }
@@ -49,14 +51,14 @@ void KMPsearch(string str, string pat) {
 }
 
 int main() {
-    string str;
-    string pat;
-    cout << "KMP algorithm: search occurences of a pattern in a string in O(n) time" << endl;
-    cout << "----------------------------------------------------------------------" << endl;
-    cout << "string:" << endl;
-    getline(cin, str);
-    cout << "pattern:" << endl;
-    getline(cin, pat);
+    std::string str;
+    std::string pat;
+    std::cout << "KMP algorithm: search occurences of a pattern in a string in O(n) time\n" 
+              << "----------------------------------------------------------------------\n"
+              << "string:" << std::endl;
+    std::getline(std::cin, str);
+    std::cout << "pattern:" << std::endl;
+    std::getline(std::cin, pat);
     KMPsearch(str, pat);
     return 0;
 }
