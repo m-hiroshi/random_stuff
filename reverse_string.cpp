c771b50a2c81468863322631f706a81166ccd666
Hao Mou <hao.mou@outlook.com>
2022-07-09 00:11:33 +0800

    Use std::reverse

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,36 +1,32 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <algorithm>
 
-inline void reverse(std::string& str, int first, int last) {
-  while (first < last) {
-    std::swap(str[first++], str[last--]);
-  }
-}
-
-void reverse_string(std::string& str) {
-  reverse(str, 0, str.size()-1);
-  for (int i = str.size()-1; i > 0; --i) {
-    if ((str[i] & 0b11100000) == 0b11000000) {
-      reverse(str, i-1, i);
-      i -= 1;
-    } else if ((str[i] & 0b11110000) == 0b11100000) {
-      reverse(str, i-2, i);
-      i -= 2;
-    } else if ((str[i] & 0b11111000) == 0b11110000) {
-      reverse(str, i-3, i);
-      i -= 3;
+void reverse_string(std::string &str) {
+    std::reverse(str.begin(), str.end());
+    auto it = str.rbegin();
+    while (it != str.rend()) {
+        if ((*it & 0b11100000) == 0b11000000) {
+            std::reverse(it, it + 2);
+            it += 2;
+        } else if ((*it & 0b11110000) == 0b11100000) {
+            std::reverse(it, it + 3);
+            it += 3;
+        } else if ((*it & 0b11111000) == 0b11110000) {
+            std::reverse(it, it + 4);
+            it += 4;
+        } else {
+            ++it;
+        }
     }
-  }
 }
 
 int main() {
-  std::string str;
-  std::getline(std::cin, str);
-  
-  reverse_string(str);
-  std::cout << str << '\n';
-  
-  return 0;
-}
+    std::string str;
+    std::getline(std::cin, str);
 
+    reverse_string(str);
+    std::cout << str << '\n';
+
+    return 0;
+}
