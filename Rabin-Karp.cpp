bd26b34a576bac7ee4f91b741abc1c0d41ee58e5
Hao Mou <hao.mou@outlook.com>
2022-07-22 19:58:13 +0800

    Use a rolling hash

--- Rabin-Karp.cpp
+++ Rabin-Karp.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <string>
-#include <string_view>
 
-void RkSearch(std::string_view str, const std::string_view pat) {
-  auto hash_func = std::hash<std::string_view>{};
-  size_t p_hash = hash_func(pat);
+class Hash {
+ public:
+  Hash(const std::string& str, size_t begin, size_t end) {
+    for (size_t ind = begin; ind < end; ++ind) { hash_ += str[ind]; } 
+  }
+
+  Hash(const std::string& str) : Hash(str, 0, str.size()) {}
+
+  inline void roll(char left, char right) {
+    hash_ = hash_ - left + right;
+  }
+
+  inline bool operator==(const Hash& that) const {
+    return hash_ == that.hash_;
+  }
+
+ private:
+  size_t hash_ = 0;
+};
+
+void RkSearch(const std::string& str, const std::string& pat) {
+  if (pat.size() > str.size()) return;
+
+  Hash p_hash = Hash(pat);
+  Hash sub_hash = Hash(str, 0, pat.size());
+
   for (int i = 0; i < str.size() - pat.size() + 1; ++i) {
-    std::string_view substr = str.substr(i, pat.size());
-    if (hash_func(substr) == p_hash && substr == pat) {
-      std::cout << "Found pattern at index " << i << "\n";
+    if (i > 0) sub_hash.roll(str[i-1], str[i-1+pat.size()]);
+    if (sub_hash == p_hash) {
+      int j = 0;
+      while (j < pat.size() && str[i+j] == pat[j]) ++j;
+      if (j == pat.size()) {
+        std::cout << "Found pattern at index " << i << "\n";
+      }
     } 
   }
 }
