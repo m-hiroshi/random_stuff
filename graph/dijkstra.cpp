a720009a006e6b03872b943386c3a8db9b85541c
mh <hao.mou@outlook.com>
2022-07-24 04:37:48 -0400

    Add Dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,11 +1,41 @@
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <limits>
+#include <set>
+#include <unordered_set>
 #include <vector>
 
 #include "util.h"
 
 std::vector<int> dijkstra(const std::vector<std::vector<int>> &adjacency) {
+  if (adjacency.empty()) return {};
+ 
+  std::vector<int> res(adjacency.size(), std::numeric_limits<int>::max());
   
+  auto cmp = [&res](int left, int right) { return res[left] < res[right]; };
+  std::set<int, decltype(cmp)> to_visit(cmp);
+  res[0] = 0;
+  to_visit.insert(0);
+
+  while (to_visit.size()) {
+    auto it = to_visit.begin();
+    int node = *it;
+    to_visit.erase(it);
+
+    const std::vector<int>& neighbors = adjacency[node];
+    for (int i = 0; i < neighbors.size(); ++i) {
+      if (i == node || neighbors[i] == std::numeric_limits<int>::max()) continue;
+      if (res[node] + neighbors[i] < res[i]) {
+        it = to_visit.find(i);
+        if (it != to_visit.end()) to_visit.erase(it);
+        res[i] = res[node] + neighbors[i];
+        to_visit.insert(i);
+      }
+    }
+  }
+
+  return res;
 }
 
 int main(int argc, char **argv) {
@@ -18,5 +48,12 @@ int main(int argc, char **argv) {
   std::ifstream graph_file(argv[1]);
   auto adjacency = load_graph_file(graph_file);
 
-  std::vector<int> shortest_paths = dijkstra(adjacency);
+  std::vector<int> shortest_dists = dijkstra(adjacency);
+  std::cout << "Shortest distance from node 0 to each node:\n";
+  for (int dist : shortest_dists) {
+    std::cout << dist << ' ';
+  }
+  std::cout << '\n';
+
+  return 0;
 }
