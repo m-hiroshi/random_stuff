d7df0d455992d54b8de3684e07276c269e9d3943
mh <hao.mou@outlook.com>
2022-07-27 02:00:50 -0400

    Morris traversal

--- tree/traversal.cpp
+++ tree/traversal.cpp
@@ -67,6 +67,68 @@ void iter_postorder(TreeNode* root) {
   std::cout << '\n';
 }
 
+void morris_inorder(TreeNode* root) {
+  TreeNode* cur = root;
+  TreeNode* tmp;
+  while (cur) {
+    if (!cur->left) {
+      std::cout << cur->val << ' ';
+      cur = cur->right;
+    } else {
+      tmp = cur->left;
+      while (tmp) {
+        if (!tmp->right || tmp->right == cur) {
+          break;
+        }
+        tmp = tmp->right;
+      }
+      if (!tmp->right) {
+        tmp->right = cur;
+        cur = cur->left;
+        continue;
+      }
+      if (tmp->right == cur) {
+        tmp->right = nullptr;
+        std::cout << cur->val << ' ';
+        cur = cur->right;
+        continue;
+      }
+    }
+  }
+  std::cout << '\n';
+}
+
+void morris_preorder(TreeNode* root) {
+  TreeNode* cur = root;
+  TreeNode* tmp;
+  while (cur) {
+    if (!cur->left) {
+      std::cout << cur->val << ' ';
+      cur = cur->right;
+    } else {
+      tmp = cur->left;
+      while (tmp) {
+        if (!tmp->right || tmp->right == cur) {
+          break;
+        }
+        tmp = tmp->right;
+      }
+      if (!tmp->right) {
+        tmp->right = cur;
+        std::cout << cur->val << ' ';
+        cur = cur->left;
+        continue;
+      }
+      if (tmp->right == cur) {
+        tmp->right = nullptr;
+        cur = cur->right;
+        continue;
+      }
+    }
+  }
+  std::cout << '\n';
+}
+
 int main() {
   /*       0
    *    1     2
@@ -76,10 +138,21 @@ int main() {
   std::vector<int> tree = 
     {0, 1, 2, 3, -1, 4, 5, -1, -1, -1, -1, -1, -1, -1, 6};
   TreeNode* root = build_tree(tree);
-  
+ 
+  std::cout << "Iterative inorder:\n"; 
   iter_inorder(root);
+
+  std::cout << "Iterative preorder:\n";
   iter_preorder(root);
+
+  std::cout << "Iterative postorder:\n";
   iter_postorder(root);
+
+  std::cout << "Morris inorder:\n";
+  morris_inorder(root);
+
+  std::cout << "Morris preorder:\n";
+  morris_preorder(root);
 
   delete_tree(root);
   return 0;
